d5460b453feed4df97a569fd30d4cb67051d0f70
Hackerbot1488 <gromovartem04@mail.ru>
2022-10-31 18:23:07 +0500

    made files for dev and prod

---
+++ about/dev.js
@@ -0,0 +1,48 @@
+const swipeWrapper = document.getElementById('about--swipe-wrapper');
+const SwipeButtonsContainer = document.getElementById('about-swipe-buttons')
+
+let activeSlideButton = SwipeButtonsContainer.getElementsByClassName('swipe-buttons__button_active')[0];
+let currentActiveSlide = 0;
+
+wrapElement(swipeWrapper);
+
+function setActiveSlideButton (button) {
+    activeSlideButton.classList.remove('swipe-buttons__button_active');
+    button.classList.add('swipe-buttons__button_active');
+    activeSlideButton = button;
+    currentActiveSlide = button.dataset.number;
+}
+
+function swipeSlide () {
+    swipeWrapper.style = `transform: translateX(calc((((50% - 45px) / 3) + 15px) * ${-currentActiveSlide}));`
+}
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 600) {
+        swipeWrapper.style = '';
+    } else {
+        swipeSlide();
+    }
+})
+
+SwipeButtonsContainer.addEventListener('click', (e) => {
+    const target = e.target;
+    if (target.classList.contains('swipe-buttons__button') & target !== activeSlideButton) {
+        setActiveSlideButton(target);
+        swipeSlide();
+    }
+})
+
+swipeWrapper.addEventListener('swipeleft', () => {
+    if (currentActiveSlide > 4) return;
+    currentActiveSlide++;
+    setActiveSlideButton(document.querySelector(`[data-position="about-${currentActiveSlide}"]`));
+    swipeSlide();
+})
+
+swipeWrapper.addEventListener('swiperight', () => {
+    if (currentActiveSlide < 1) return;
+    currentActiveSlide--;
+    setActiveSlideButton(document.querySelector(`[data-position="about-${currentActiveSlide}"]`));
+    swipeSlide();
+})
